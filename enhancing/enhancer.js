aa554887b2d5c8ab2245f9e05df24edacafb939e
Brian Abeyta-Pratt <babeytapratt@gmail.com>
2020-12-16 16:00:47 -0700

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,32 @@ module.exports = {
 };
 
 function success(item) {
+  if (item.enhancement < 20) {
+    item.enhancement += 1
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability -= 5
+  } else if
+    (item.enhancement > 15) {
+      item.durability -= 10
+    } else if
+      (item.enhancement > 16) {
+        item.durability -= 1
+      }
   return { ...item };
 }
 
+
+
+
 function repair(item) {
+  if (item.durability < 100){
+    item.durability = 100
+  }
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,43 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe('Sanity and intro', () => {
+    it('works', () => {
+        expect(2 + 2).toBe(4)
+    })
+    test('everything working', () => {
+        expect({}).not.toBe({})
+        expect({}).toEqual({})
+    })
+})
+
+describe('Enhancer', () => {
+    beforeEach(() => {
+        item = { name: 'broadsword',
+                durability: 70,
+                enhancement: 10
+                }
+    })
+    it('item is defined', () => {
+        expect(item).toBeDefined()
+    })
+    it('durability is defined', () => {
+        expect(item.durability).toBeDefined()
+        expect(item.durability).toBe(70)
+        })
+    it('enhancement is defined', () => {
+        expect(item.enhancement).toBeDefined()
+        expect(item.enhancement).toBe(10)
+        })
+    it('can restore the durability back to 100', () => {
+        enhancer.repair(item)
+        expect(item.durability).toBe(100)
+    })
+    it('success method works', () => {
+        enhancer.success(item)
+        expect(item.enhancement).toBe(11)
+    })
+    it('fail method works', () => {
+        enhancer.fail(item)
+        expect(item.durability).toBe(65)
+    })
+})
